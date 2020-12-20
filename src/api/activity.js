e7f0442e06499dd95278019dfe6f660d6c4e465c
Alexander Bos <abos@revridge.com>
2020-12-20 17:36:24 -0500

    12/19 React PseudoCode
    Commit in attempt to set up deploy

--- src/api/activity.js
+++ src/api/activity.js
@@ -4,15 +4,51 @@ import apiUrl from '../apiConfig'
 export const createActivity = (data, user) => {
   return axios({
     method: 'POST',
-    url: apiUrl + '/create/',
+    url: apiUrl + '/activities/',
     headers: {
       'Authorization': `Token ${user.token}`
     },
-    data: {
-      name: data.name,
-      activity: data.activity,
-      description: data.description,
-      note: data.note
+    data: { data }
+  })
+}
+
+export const indexActivities = (user) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/activities/',
+    headers: {
+      'Authorization': `Token ${user.token}`
+    }
+  })
+}
+
+export const showActivity = (user, activityId) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/activities/' + activityId,
+    headers: {
+      'Authorization': `Token ${user.token}`
+    }
+  })
+}
+
+export const updateActivity = (user, data, activityId) => {
+  return axios({
+    method: 'PATCH',
+    url: apiUrl + '/activities/' + activityId,
+    headers: {
+      'Authorization': `Token ${user.token}`
+    },
+    data: { data }
+  })
+}
+
+export const deleteActivity = (user, activityId) => {
+  return axios({
+    method: 'DELETE',
+    url: apiUrl + '/activities/' + activityId,
+    headers: {
+      'Authorization': `Token ${user.token}`
     }
   })
 }
